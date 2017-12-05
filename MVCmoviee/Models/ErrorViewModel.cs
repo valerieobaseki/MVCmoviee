2d7e2e50cdb1d43c12481fc04916976b0b2104b0
Valerie Obaseki <valerieobaseki@Valeries-Air.hitronhub.home>
2017-12-05 10:42:50 -0600

    Initial commit

---
+++ MVCmoviee/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCmoviee.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
